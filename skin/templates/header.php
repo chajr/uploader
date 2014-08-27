234a0a4c148d564ef9e768405a644b3247271f08
chajr <git@bluetree.pl>
2014-08-27 21:59:59 +0200

    mobile version
    This version introduces multiple enhancements:
    
    changed css to make responsive version
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   skin/templates/header.php
                -- Added --
                    meta viewport
    
    Modified:   skin/css/mobile.css
                -- Added --
                    responsive styles

--- skin/templates/header.php
+++ skin/templates/header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
         <title>File uploader</title>
         <link rel="stylesheet" href="/skin/css/bootstrap.css"/>
         <link rel="stylesheet" href="/skin/css/bootstrap_dark.css"/>
